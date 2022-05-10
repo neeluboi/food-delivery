2823486ae61f7df910df3948f0c5f4d2627c4519
neeluboi <55922407+neeluboi@users.noreply.github.com>
2022-05-11 01:28:22 +0530

    Add files via upload

---
+++ OnlinePizzaOrderingSystem-master/models/pizza.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose')
+var pizzaSchema = mongoose.Schema({
+    
+    pizzaname: {
+        type: String,
+      
+    },
+    pizzasize: {
+        type: String,
+      
+    },
+    pizzaprice: {
+        type: Number,
+      
+    },
+    pizzaimage: {
+        type: String,
+        default:'',
+    },
+    createdAt: {type: Date, default: Date.now}
+})
+module.exports = mongoose.model('pizza',pizzaSchema)
+
